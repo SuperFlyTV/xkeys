2a7bd6123e8d303d37378ff332374ba732dbb645
superflytvab <info@superfly.tv>
2020-10-27 12:59:42 +0000

    Updates

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"65536":"Type literal"},"rows":[{"id":0,"kind":65536,"name":"__type","url":"globals.html#buttonstates.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported","parent":"ButtonStates"}]};
