2de8bf67b433232c1524d1151d4074cbd759d04e
Johan Nyman <johan@nytamin.se>
2021-03-21 19:42:16 +0100

    chore: script for building record-test.ts into executable

--- src/lib.ts
+++ src/lib.ts
@@ -25,3 +25,47 @@ export function isHID_HID(device: HID.Device | HID.HID | string): device is HID_
 		(device as HID_HID).devicePath !== undefined // yes, HID.HID exposes this, we're using that
 	)
 }
+
+export function describeEvent(event: string, args: any[]): string {
+	const metadataStr = (metadata: any) => {
+		const strs: string[] = []
+		Object.entries(metadata).forEach(([key, value]) => {
+			strs.push(`${key}: ${value}`)
+		})
+		return strs.join(', ')
+	}
+
+	if (event === 'down') {
+		const btnIndex = args[0]
+		const metadata = args[1]
+		return `Button ${btnIndex} pressed.  Metadata: ${metadataStr(metadata)}`
+	} else if (event === 'up') {
+		const btnIndex = args[0]
+		const metadata = args[1]
+		return `Button ${btnIndex} released. Metadata: ${metadataStr(metadata)}`
+	} else if (event === 'jog') {
+		const index = args[0]
+		const value = args[1]
+		const metadata = args[2]
+		return `Jog ( index ${index}) value: ${value}. Metadata: ${metadataStr(metadata)}`
+	} else if (event === 'shuttle') {
+		const index = args[0]
+		const value = args[1]
+		const metadata = args[2]
+		return `Shuttle ( index ${index}) value: ${value}. Metadata: ${metadataStr(metadata)}`
+	} else if (event === 'joystick') {
+		const index = args[0]
+		const value = args[1]
+		const metadata = args[2]
+		return `Joystick ( index ${index}) value: ${value}. Metadata: ${metadataStr(metadata)}`
+	} else if (event === 'tbar') {
+		const index = args[0]
+		const value = args[1]
+		const metadata = args[2]
+		return `T-bar ( index ${index}) value: ${value}. Metadata: ${metadataStr(metadata)}`
+	} else if (event === 'disconnected') {
+		return `Panel disconnected!`
+	}
+
+	throw new Error('Unhnandled event!')
+}
