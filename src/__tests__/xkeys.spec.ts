df7d9b63b2b23d7833da0cb8f7e0e9f2a7c5f524
Johan Nyman <johan@nytamin.se>
2021-04-07 07:38:42 +0200

    chore: unit tests for XKeys.calculateDelta()

--- src/__tests__/xkeys.spec.ts
+++ src/__tests__/xkeys.spec.ts
@@ -152,6 +152,17 @@ describe('Recorded tests', () => {
 		})
 	})
 })
+describe('Unit tests', () => {
+	test('calculateDelta', () => {
+		expect(XKeys.calculateDelta(100, 100)).toBe(0)
+		expect(XKeys.calculateDelta(110, 100)).toBe(10)
+		expect(XKeys.calculateDelta(90, 100)).toBe(-10)
+		expect(XKeys.calculateDelta(0, 255)).toBe(1)
+		expect(XKeys.calculateDelta(5, 250)).toBe(11)
+		expect(XKeys.calculateDelta(255, 0)).toBe(-1)
+		expect(XKeys.calculateDelta(250, 5)).toBe(-11)
+	})
+})
 
 /** Data sent to the panel */
 let sentData: string[] = []
